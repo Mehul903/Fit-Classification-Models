da69dfb44f13fa73af4a59014185805d826083db
Mehul903 <patel.mehu@husky.neu.edu>
2018-01-02 17:31:24 -0500

    Modified script to reflect changes of module.

--- class/test_script.py
+++ class/test_script.py
@@ -1,8 +1,6 @@
-# -*- coding: utf-8 -*-
 """
-Created on Sat Dec 23 14:42:55 2017
-
 @author: MPatel
+
 """
 
 import SupervisedModels as sm
@@ -10,23 +8,78 @@ import pandas as pd
 import numpy as np
 
 
+
+### If input data is in numpy-matrix form:
 ## Importing the dataset
 dataset = pd.read_csv('../data/Churn_Modelling.csv')
 X = dataset.iloc[:, 3:13].values  ## Removing unnecessary columns
 y = dataset.iloc[:, 13].values
 
-### Remove one of the dummy columns of country variable to avoid dummy variable trap:
-#X = X[:, 1:]
+## Fitting models using my module!
+LR = sm.SupervisedClassificationModels(predictors = X, outcome = y, 
+                                       test_frac = 0.2, col_ind = [1,2], 
+                                       class_report = True, matrix = True)
+lr, cm, cr = LR.fit_logistic_regression()
+print ('Confusion matrix for Logistic-Regression: \n', cm)
+print ('Classification report for Logistic-Regression: \n', cr)
 
+
+RF = sm.SupervisedClassificationModels(X, y, 0.2, [1,2], class_report = True, matrix = True)
+rf, cm, cr = RF.fit_random_forest()
+print ('Confusion matrix for Random-Forest: \n', cm)
+print ('Classification report for Random-Forest: \n', cr)
+
+
+SV = sm.SupervisedClassificationModels(X, y, 0.2, [1,2], class_report = True, matrix = True)
+SV, cm, cr = SV.fit_support_vector_classifier()
+print ('Confusion matrix for Support-Vector-Classifier: \n', cm)
+print ('Classification report for Support-Vector-Classifier: \n', cr)
+
+
+
+### If input data is dataframe:
+## Importing the dataset
+dataset = pd.read_csv('../data/Churn_Modelling.csv')
+X = dataset.iloc[:, 3:13]  ## Removing unnecessary columns
+y = dataset.iloc[:, 13]
 
 ## Fitting models using my module!
-LR = SupervisedClassificationModels(predictors = X, outcome = y, 
-                                    test_frac = 0.2, col_ind = [1,2], 
-                                    class_report = True)
+LR = sm.SupervisedClassificationModels(predictors = X, outcome = y, 
+                                       test_frac = 0.2, col_ind = [1,2], 
+                                       class_report = True)
 lr, cm, cr = LR.fit_logistic_regression()
+print ('Confusion matrix for Logistic-Regression: \n', cm)
+print ('Classification report for Logistic-Regression: \n', cr)
 
-RF = SupervisedClassificationModels(X, y, 0.2, [1,2], class_report = True)
+
+RF = sm.SupervisedClassificationModels(predictors = X, outcome = y, 
+                                       test_frac = 0.2, col_ind = [1,2], 
+                                       class_report = True)
 rf, cm, cr = RF.fit_random_forest()
+print ('Confusion matrix for Random-Forest: \n', cm)
+print ('Classification report for Random-Forest: \n', cr)
 
-SV = SupervisedClassificationModels(X, y, 0.2, [1,2], class_report = True)
-SV, cm, cr = RF.fit_support_vector_classifier()
+
+SV = sm.SupervisedClassificationModels(predictors = X, outcome = y, 
+                                       test_frac = 0.2, col_ind = [1,2], 
+                                       class_report = True)
+SV, cm, cr = SV.fit_support_vector_classifier()
+print ('Confusion matrix for Support-Vector-Classifier: \n', cm)
+print ('Classification report for Support-Vector-Classifier: \n', cr)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
