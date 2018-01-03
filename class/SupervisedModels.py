29e64ded9490158854e729d8fec361a5edab1972
Mehul Rakeshkumar Patel <patel.mehu@northeastern.edu>
2018-01-03 13:55:49 -0500

    Added function for RF-feature importance plot.

--- class/SupervisedModels.py
+++ class/SupervisedModels.py
@@ -5,6 +5,7 @@
 
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 from sklearn.model_selection import train_test_split
@@ -44,6 +45,8 @@ class SupervisedClassificationModels:
         self._class_report = class_report
         self._matrix = matrix
         self._predictors_temp = None
+        self._rf = None  ## A Random-Forest object
+        
         
     def _feature_engineering(self):
         """
@@ -91,8 +94,7 @@ class SupervisedClassificationModels:
     
             return self._predictors    
 
-    
-    
+        
     def _train_test_split(self):
         """
         Split the data into train-test set for further modeling process. Returns a tuple of 
@@ -145,7 +147,6 @@ class SupervisedClassificationModels:
         cm = confusion_matrix(y_pred = y_pred, y_true = y_test)
        
         p,r,f,s = precision_recall_fscore_support(y_true = y_test, y_pred = y_pred)
-        print ('Done.')
         
         if self._class_report:
             
@@ -180,9 +181,9 @@ class SupervisedClassificationModels:
         X_test = scaler.transform(X_test)
         
         ##Fitting a model:
-        rf = RandomForestClassifier(random_state = 101)
-        rf = rf.fit(X_train, y_train)
-        y_pred = rf.predict(X_test)
+        self._rf = RandomForestClassifier(random_state = 101)
+        self._rf = self._rf.fit(X_train, y_train)
+        y_pred = self._rf.predict(X_test)
         cm = confusion_matrix(y_pred = y_pred, y_true = y_test)
        
         p,r,f,s = precision_recall_fscore_support(y_true = y_test, y_pred = y_pred)
@@ -194,10 +195,29 @@ class SupervisedClassificationModels:
                                                   'F_score': f, 
                                                   'Support': s, 
                                                   'Class': np.unique(y_test)})
-            return rf, cm, classification_report 
+            return self._rf, cm, classification_report 
         
-        return rf, cm
+        return self._rf, cm
 
+
+    def plot_feature_importance(self):
+        
+        ## Plot feature importance for Random-Forest:
+        feature_importance = self._rf.feature_importances_
+        features = self._predictors.columns
+        
+        ft_imp_df = pd.DataFrame({'Features': features, 'Feature_Importance': feature_importance})
+        ft_imp_df.sort_values(by = 'Feature_Importance', ascending = False, inplace = True)
+        ft_imp_df.reset_index(inplace = True, drop = True)
+        
+        fig, ax = plt.subplots(figsize = (12,6))
+        ft_imp_df.plot(y = 'Feature_Importance', x = 'Features', ax = ax, kind = 'barh')
+        ax.set_xlabel('Relative Importance of Features')
+        ax.set_ylabel('Features')
+        ax.set_title('RF: Feature Importance Plot', fontsize = 20)
+        
+        
+        return fig, ax
 
     def fit_support_vector_classifier(self):
         """
@@ -243,12 +263,35 @@ class SupervisedClassificationModels:
 # =============================================================================
 # ## Importing the dataset
 # dataset = pd.read_csv('../data/Churn_Modelling.csv')
-# #X = dataset.iloc[:, 3:13]   ## Removing unnecessary columns
-# #y = dataset.iloc[:, 13]
+# X = dataset.iloc[:, 3:13]   ## Removing unnecessary columns
+# y = dataset.iloc[:, 13]
 #   
-# X = dataset.iloc[:, 3:13].values  ## Removing unnecessary columns
-# y = dataset.iloc[:, 13].values
+# #X = dataset.iloc[:, 3:13].values  ## Removing unnecessary columns
+# #y = dataset.iloc[:, 13].values
 # 
+# col_ind = [1,2]
+# cat_data = X.iloc[:, col_ind]
+# 
+# 
+# X_temp = X.drop(X.columns[col_ind], axis = 1)
+# original_col_names = list(X_temp.columns)
+# X = pd.DataFrame()
+# 
+# 
+# dummy_col_names = []
+# for col in col_ind:
+#     LE = LabelEncoder()
+#     cat_data.iloc[:, col-1] = LE.fit_transform(list(cat_data.iloc[:, col-1]))
+#     
+#     new_names_temp = [LE.classes_[i]+'_'+str(i) for i in range(len(LE.classes_))]    
+#     dummy_col_names.extend(new_names_temp)
+# 
+# 
+# OHE = OneHotEncoder(sparse = False)
+# cat_data = OHE.fit_transform(cat_data)    
+# cat_data = pd.DataFrame(cat_data, columns = dummy_col_names)
+# 
+# X = pd.concat([X_temp, cat_data], axis = 1)
 # 
 # 
 # ## Merge above dataframe with original dataframe:
@@ -258,21 +301,46 @@ class SupervisedClassificationModels:
 # #final_col_names.extend(new_col_names)
 #   
 # 
-# LR = SupervisedClassificationModels(predictors = X, outcome = y, 
-#                                       test_frac = 0.2, col_ind = [1,2], 
-#                                       class_report = True, matrix=True)
-# lr, cm, cr = LR.fit_logistic_regression()
+# #LR = SupervisedClassificationModels(predictors = X, outcome = y, 
+# #                                      test_frac = 0.2, col_ind = [1,2], 
+# #                                      class_report = True, matrix=True)
+# #lr, cm, cr = LR.fit_logistic_regression()
 #   
 # 
-# RF = SupervisedClassificationModels(X, y, 0.2, [1,2], class_report = True, matrix = True)
+# RF = SupervisedClassificationModels(X, y, 0.2, [1,2], class_report = True, matrix = False)
 # rf, cm, cr = RF.fit_random_forest()
+# fig, ax = RF.plot_feature_importance()
+# 
+# ### Plot feature importance for Random-Forest:
+# #feature_importance = rf.feature_importances_
+# #features = X.columns
+# #
+# #ft_imp_df = pd.DataFrame({'Features': features, 'Feature_Importance': feature_importance})
+# #ft_imp_df.sort_values(by = 'Feature_Importance', ascending = False, inplace = True)
+# #ft_imp_df.reset_index(inplace = True, drop = True)
+# #
+# #fig, ax = plt.subplots(figsize = (12,6))
+# #ft_imp_df.plot(y = 'Feature_Importance', x = 'Features', ax = ax, kind = 'barh')
+# #ax.set_xlabel('Relative Importance of Features')
+# #ax.set_ylabel('Features')
+# #ax.set_title('RF: Feature Importance Plot', fontsize = 20)
+# #plt.show()
 # 
 # 
-# SV = SupervisedClassificationModels(X, y, 0.2, [1,2], class_report = True, matrix = True)
-# SV, cm, cr = SV.fit_support_vector_classifier()
+# ### Plot tree of a Random-Forest:
+# #from sklearn.tree import export_graphviz
+# #import graphviz
+# #
+# #dot_data = export_graphviz(rf.estimators_[0], out_file = None, max_depth=5, feature_names = X.columns)
+# #graph = graphviz.Source(dot_data)  
+# #graph
+# 
+# #SV = SupervisedClassificationModels(X, y, 0.2, [1,2], class_report = True, matrix = True)
+# #SV, cm, cr = SV.fit_support_vector_classifier()
 #   
 # ## Remove one of the dummy columns of country variable to avoid dummy variable trap:
 # #X = X[:, 1:]
 #         
+#     
+# 
 # =============================================================================
-    
